cf4f3da70b9317df08e6e4363c6aec0ef9272d18
infected521 <30352728+infected521@users.noreply.github.com>
2019-10-31 13:19:01 -0600

    Update Crear-Demo.sh

--- gerador/Crear-Demo.sh
+++ gerador/Crear-Demo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 Block="/etc/nanobc" && [[ ! -d ${Block} ]] && exit
-Block > /dev/null 2>&1
-
-SCPdir="/etc/newadm"
+3
+Block > /dev/null 2>&1SCPdir="/etc/newadm"
 SCPusr="${SCPdir}/ger-user"
 SCPfrm="/etc/ger-frm"
 SCPfrm3="/etc/adm-lite"

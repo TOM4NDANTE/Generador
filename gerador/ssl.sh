042d8f58d2ff764785b8d46f637770f013e40ef9
infected521 <30352728+infected521@users.noreply.github.com>
2019-11-09 17:39:37 -0600

    Add files via upload

--- gerador/ssl.sh
+++ gerador/ssl.sh
@@ -13,7 +13,7 @@ SCPidioma="${SCPdir}/idioma"
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
 SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
-API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9sZnlxZGI5NnJkejl5bW8vdHJhbnM/ZGw9MA=="
+API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy80bW5tcjdpOXRpYmc5dGcvdHJhbnMK"
 SUB_DOM='base64 -d'
 wget -O /usr/bin/trans $(echo $API_TRANS|$SUB_DOM) &> /dev/null
 mportas () {
@@ -28,51 +28,126 @@ echo -e "$portas"
 }
 ssl_stunel () {
 [[ $(mportas|grep stunnel4|head -1) ]] && {
-msg -ama " $(fun_trans "Parando Stunnel")"
+echo -e "\033[1;33m $(fun_trans  "Parando Stunnel")"
 msg -bar
-fun_bar "service stunnel4 stop"
+fun_bar "apt-get purge stunnel4 -y"
 msg -bar
-msg -ama " $(fun_trans "Parado Con exito!")"
+echo -e "\033[1;33m $(fun_trans  "Parado Con Exito!")"
 msg -bar
 return 0
 }
-msg -azu " $(fun_trans "SSL Stunnel")"
+echo -e "\033[1;32m $(fun_trans  "INSTALADOR SSL LA CASITA DEL TERROR")"
 msg -bar
-msg -ama " $(fun_trans "Seleccione una puerta de redirección interna")"
-msg -ama " $(fun_trans "Es decir, un puerto en su servidor para SSL | se recomienda que pongas tu puerto ssh elemplo 22")"
+echo -e "\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
+echo -e "\033[1;33m $(fun_trans  "Es decir, un puerto en su servidor para SSL")"
 msg -bar
          while true; do
          echo -ne "\033[1;37m"
          read -p " Local-Port: " portx
          if [[ ! -z $portx ]]; then
              if [[ $(echo $portx|grep [0-9]) ]]; then
-                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans "Puerto Invalido")"
+                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans  "Puerta invalida")"
              fi
          fi
          done
 msg -bar
 DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
-msg -ama " $(fun_trans "Ahora precisamos Saber que puerta SSL, va a escuchar")"
+echo -e "\033[1;33m $(fun_trans  "Ahora Prestamos Saber Que Puerta del SSL, Va a Escuchar")"
 msg -bar
     while true; do
     read -p " Listen-SSL: " SSLPORT
     [[ $(mportas|grep -w "$SSLPORT") ]] || break
-    echo -e "\033[1;33m $(fun_trans "esta puerta está en uso")"
+    echo -e "\033[1;33m $(fun_trans  "Esta puerta está en uso")"
     unset SSLPORT
     done
 msg -bar
-msg -ama " $(fun_trans "Instalando SSL")"
+echo -e "\033[1;33m $(fun_trans  "Instalando SSL")"
 msg -bar
 fun_bar "apt-get install stunnel4 -y"
-echo -e "cert = /etc/stunnel/stunnel.pem\nclient = no\nsocket = a:SO_REUSEADDR=1\nsocket = l:TCP_NODELAY=1\nsocket = r:TCP_NODELAY=1\n\n[stunnel]\nconnect = 127.0.0.1:${DPORT}\naccept = ${SSLPORT}" > /etc/stunnel/stunnel.conf
-openssl genrsa -out key.pem 2048 > /dev/null 2>&1
-(echo MX; echo MX; echo MX; echo speed; echo adm; echo ultimate; echo @lacasitadelterror)|openssl req -new -x509 -key key.pem -out cert.pem -days 1095 > /dev/null 2>&1
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" > /etc/stunnel/stunnel.conf
+####Coreccion2.0##### 
+openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
+
+(echo "La" ; echo "Ca" ; echo "si" ; echo "ta" ; echo "del" ; echo "te" ; echo "rror" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
+
+cat stunnel.crt stunnel.key > stunnel.pem 
+
+mv stunnel.pem /etc/stunnel/
+######-------
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 service stunnel4 restart > /dev/null 2>&1
 msg -bar
-msg -ama " $(fun_trans "INSTALADO CON EXITO")"
+echo -e "\033[1;33m $(fun_trans  "INSTALADO CON EXITO")"
 msg -bar
+rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
+rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
+rm -rf /root/stunnel.crt > /dev/null 2>&1
+rm -rf /root/stunnel.key > /dev/null 2>&1
 return 0
 }
+ssl_stunel_2 () {
+echo -e "\033[1;32m $(fun_trans  "INSTALADOR SSL LA CASITA DEL TERROR")"
+msg -bar
+echo -e "\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
+echo -e "\033[1;33m $(fun_trans  "Es decir, un puerto en su servidor para SSL")"
+msg -bar
+         while true; do
+         echo -ne "\033[1;37m"
+         read -p " Local-Port: " portx
+         if [[ ! -z $portx ]]; then
+             if [[ $(echo $portx|grep [0-9]) ]]; then
+                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans  "Puerta invalida")"
+             fi
+         fi
+         done
+msg -bar
+DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
+echo -e "\033[1;33m $(fun_trans  "Ahora Prestamos Saber Que Puerta del SSL, Va a Escuchar")"
+msg -bar
+    while true; do
+    read -p " Listen-SSL: " SSLPORT
+    [[ $(mportas|grep -w "$SSLPORT") ]] || break
+    echo -e "\033[1;33m $(fun_trans  "Esta puerta está en uso")"
+    unset SSLPORT
+    done
+msg -bar
+echo -e "\033[1;33m $(fun_trans  "Instalando SSL")"
+msg -bar
+fun_bar "apt-get install stunnel4 -y"
+echo -e "client = no\n[SSL+]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" >> /etc/stunnel/stunnel.conf
+######-------
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart > /dev/null 2>&1
+msg -bar
+echo -e "\033[1;33m $(fun_trans  "INSTALADO CON EXITO")"
+msg -bar
+rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
+rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
+rm -rf /root/stunnel.crt > /dev/null 2>&1
+rm -rf /root/stunnel.key > /dev/null 2>&1
+return 0
+}
+echo -e "${cor[3]}INSTALADOR SSL LA CASITA DEL TERROR"
+msg -bar
+echo -e "${cor[1]} Escoja la opcion deseada."
+msg -bar
+echo "1).- ININICIAR O PARAR SSL"
+echo "2).- AGREGAR PUERTOS SSL"
+msg -bar
+echo -n "Digite solo el numero segun su respuesta: "
+read opcao
+case $opcao in
+1)
+msg -bar
 ssl_stunel
+;;
+2)
+msg -bar
+echo -e "\033[1;93m  AGREGAR SSL EXTRA  ..."
+msg -bar
+ssl_stunel_2
+sleep 3
+exit
+;;
+esac
+
